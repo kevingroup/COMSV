071229dab7cf29f4c1e644b5ac1a21224cdaa05a
kevingroup <39295240+kevingroup@users.noreply.github.com>
2023-05-15 13:21:27 -0400

    common header file for c++ source codes

---
+++ source/Header.h
@@ -0,0 +1,33 @@
+#ifndef NOERROR_H
+#define NOERROR_H
+#include<cstdio>
+#include<exception>
+#include<functional>
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<queue>
+#include<map>
+#include<cmath>
+#include<vector>
+#include<set>
+#include<random>
+#include<ctime>
+#define pi 3.1415926535897
+#define eps 1e-8
+#define oo64 (((LL)1)<<59)
+//#define FILENAME "/local/shared/Human/hg19.clean.fa"
+#define DNAMAX (long long)3157608038
+//#define segmentLength 150000
+#define hashTableSizeOrigin (long long)179424907
+#define numberOfChromosome 25
+#define sqr(x) ((x)*(x))
+typedef long long LL;
+
+using namespace std;
+
+#endif
